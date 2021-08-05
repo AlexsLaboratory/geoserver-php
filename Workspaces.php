ab6591bac15faf4e80c24f737a21dd81bc49dced
Alex Lowe <loweman64@outlook.com>
2021-08-05 15:55:55 -0400

    feat: Create All Methods for Geoserver Workspaces

--- Workspaces.php
+++ Workspaces.php
@@ -11,8 +11,59 @@ class Workspaces extends Wrapper {
     $curl = new EasyCurl($this->getBaseURL() . "/workspaces");
     $curl->setBasicAuth($this->getUsername(), $this->getPassword());
     $curl->get();
-    if ($curl->getErrorMessage()) {
-      return $curl->getErrorMessage();
+    if ($curl->getErrorCode()) {
+      return $curl->getErrorCode();
+    }
+    return $curl->getExecMessage();
+  }
+
+  public function create($workspaceName): string {
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $curl->post("<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<workspace>
+    <name>{$workspaceName}</name>
+</workspace>", [
+      "Content-Type: application/xml"
+    ]);
+    if ($curl->getErrorCode()) {
+      return $curl->getErrorCode();
+    }
+    return $curl->getExecMessage();
+  }
+
+  public function get($workspaceName): string {
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/{$workspaceName}");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $curl->get();
+    if ($curl->getErrorCode()) {
+      return $curl->getErrorCode();
+    }
+    return $curl->getExecMessage();
+  }
+
+  public function update($currentWorkspaceName, $newWorkspaceName): string {
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/{$currentWorkspaceName}");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $curl->put("<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<workspace>
+    <name>{$newWorkspaceName}</name>
+</workspace>", [
+      "Content-Type: application/xml"
+    ]);
+    if ($curl->getErrorCode()) {
+      return $curl->getErrorCode();
+    }
+    return $curl->getExecMessage();
+  }
+
+  public function delete($workspaceName, $recurse = FALSE): string {
+    $recurseString = $recurse ? "true" : "false";
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/{$workspaceName}?recurse={$recurseString}");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $curl->delete();
+    if ($curl->getErrorCode()) {
+      return $curl->getErrorCode();
     }
     return $curl->getExecMessage();
   }
