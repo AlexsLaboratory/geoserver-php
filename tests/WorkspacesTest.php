62e1587f587155ef0b74cb753a052d255ce1848c
Alex Lowe <loweman64@outlook.com>
2021-08-05 15:56:35 -0400

    test: Add All Tests for Workspace Methods

--- tests/WorkspacesTest.php
+++ tests/WorkspacesTest.php
@@ -8,7 +8,36 @@ class WorkspacesTest extends TestCase {
     $workspace = new Workspaces("http://192.168.160.137:8080");
     $workspace->setBasicAuth("admin", "geoserver");
     $result = $workspace->getAll();
-    $this->assertNotEmpty($result);
+    self::assertIsNotNumeric($result);
     print_r($result);
+  }
+
+  public function testCreate() {
+    $workspace = new Workspaces("http://192.168.160.137:8080");
+    $workspace->setBasicAuth("admin", "geoserver");
+    $result = $workspace->create("Test1");
+    self::assertIsNotNumeric($result);
+  }
+
+  public function testGet() {
+    $workspace = new Workspaces("http://192.168.160.137:8080");
+    $workspace->setBasicAuth("admin", "geoserver");
+    $result = $workspace->get("Test1");
+    self::assertIsNotNumeric($result);
+    print_r($result);
+  }
+
+  public function testUpdate() {
+    $workspace = new Workspaces("http://192.168.160.137:8080");
+    $workspace->setBasicAuth("admin", "geoserver");
+    $result = $workspace->update("Test1", "Test12");
+    self::assertIsNotNumeric($result);
+  }
+
+  public function testDelete() {
+    $workspace = new Workspaces("http://192.168.160.137:8080");
+    $workspace->setBasicAuth("admin", "geoserver");
+    $result = $workspace->delete("Test12", TRUE);
+    self::assertIsNotNumeric($result);
   }
 }
