5e2f8cd9c2ce70b0ad63eb95a8ba29fdf3e0aed1
Alex Lowe <loweman64@outlook.com>
2021-08-06 08:26:29 -0400

    feat: Update easy-curl to Version 3.0

--- Workspaces.php
+++ Workspaces.php
@@ -3,20 +3,24 @@
 namespace Lowem\GeoserverPHP;
 
 use Lowem\EasyCurl\EasyCurl;
+use Lowem\EasyCurl\HTTPRequestException;
 
 class Workspaces extends Wrapper {
   public function __construct($baseURL) { parent::__construct($baseURL); }
 
+  /**
+   * @throws HTTPRequestException
+   */
   public function getAll(): string {
     $curl = new EasyCurl($this->getBaseURL() . "/workspaces");
     $curl->setBasicAuth($this->getUsername(), $this->getPassword());
     $curl->get();
-    if ($curl->getErrorCode()) {
-      return $curl->getErrorCode();
-    }
     return $curl->getExecMessage();
   }
 
+  /**
+   * @throws HTTPRequestException
+   */
   public function create($workspaceName): string {
     $curl = new EasyCurl($this->getBaseURL() . "/workspaces");
     $curl->setBasicAuth($this->getUsername(), $this->getPassword());
@@ -26,22 +30,22 @@ class Workspaces extends Wrapper {
 </workspace>", [
       "Content-Type: application/xml"
     ]);
-    if ($curl->getErrorCode()) {
-      return $curl->getErrorCode();
-    }
     return $curl->getExecMessage();
   }
 
+  /**
+   * @throws HTTPRequestException
+   */
   public function get($workspaceName): string {
     $curl = new EasyCurl($this->getBaseURL() . "/workspaces/{$workspaceName}");
     $curl->setBasicAuth($this->getUsername(), $this->getPassword());
     $curl->get();
-    if ($curl->getErrorCode()) {
-      return $curl->getErrorCode();
-    }
     return $curl->getExecMessage();
   }
 
+  /**
+   * @throws HTTPRequestException
+   */
   public function update($currentWorkspaceName, $newWorkspaceName): string {
     $curl = new EasyCurl($this->getBaseURL() . "/workspaces/{$currentWorkspaceName}");
     $curl->setBasicAuth($this->getUsername(), $this->getPassword());
@@ -51,20 +55,17 @@ class Workspaces extends Wrapper {
 </workspace>", [
       "Content-Type: application/xml"
     ]);
-    if ($curl->getErrorCode()) {
-      return $curl->getErrorCode();
-    }
     return $curl->getExecMessage();
   }
 
+  /**
+   * @throws HTTPRequestException
+   */
   public function delete($workspaceName, $recurse = FALSE): string {
     $recurseString = $recurse ? "true" : "false";
     $curl = new EasyCurl($this->getBaseURL() . "/workspaces/{$workspaceName}?recurse={$recurseString}");
     $curl->setBasicAuth($this->getUsername(), $this->getPassword());
     $curl->delete();
-    if ($curl->getErrorCode()) {
-      return $curl->getErrorCode();
-    }
     return $curl->getExecMessage();
   }
 }
