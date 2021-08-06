5325e5d589789b21c35745c068f37bcd55c527cc
Alex Lowe <loweman64@outlook.com>
2021-08-06 09:12:12 -0400

    test: Update Tests for Version 3.0 of easy-curl

--- tests/WorkspacesTest.php
+++ tests/WorkspacesTest.php
@@ -2,42 +2,75 @@
 
 use Lowem\GeoserverPHP\Workspaces;
 use PHPUnit\Framework\TestCase;
+use Lowem\EasyCurl\HTTPRequestException;
 
 class WorkspacesTest extends TestCase {
   public function testGetAll() {
     $workspace = new Workspaces("http://192.168.160.137:8080");
     $workspace->setBasicAuth("admin", "geoserver");
-    $result = $workspace->getAll();
-    self::assertIsNotNumeric($result);
+    $error = FALSE;
+    $result = "";
+    try {
+      $result = $workspace->getAll();
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
     print_r($result);
+    self::assertFalse($error);
   }
 
   public function testCreate() {
     $workspace = new Workspaces("http://192.168.160.137:8080");
     $workspace->setBasicAuth("admin", "geoserver");
-    $result = $workspace->create("Test1");
-    self::assertIsNotNumeric($result);
+    $error = FALSE;
+    try {
+      $workspace->create("Test1");
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error);
   }
 
   public function testGet() {
     $workspace = new Workspaces("http://192.168.160.137:8080");
     $workspace->setBasicAuth("admin", "geoserver");
-    $result = $workspace->get("Test1");
-    self::assertIsNotNumeric($result);
+    $error = FALSE;
+    $result = "";
+    try {
+      $result = $workspace->get("Test1");
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
     print_r($result);
+    self::assertFalse($error);
   }
 
   public function testUpdate() {
     $workspace = new Workspaces("http://192.168.160.137:8080");
     $workspace->setBasicAuth("admin", "geoserver");
-    $result = $workspace->update("Test1", "Test12");
-    self::assertIsNotNumeric($result);
+    $error = FALSE;
+    try {
+      $workspace->update("Test1", "Test12");
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error);
   }
 
   public function testDelete() {
     $workspace = new Workspaces("http://192.168.160.137:8080");
     $workspace->setBasicAuth("admin", "geoserver");
-    $result = $workspace->delete("Test12", TRUE);
-    self::assertIsNotNumeric($result);
+    $error = FALSE;
+    try {
+      $result = $workspace->delete("Test12", TRUE);
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error);
   }
 }
