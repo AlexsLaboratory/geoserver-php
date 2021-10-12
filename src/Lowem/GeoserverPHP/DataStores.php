7ffb0e0549003048e63bdfff448fa9d81dda2c63
Alex Lowe <loweman64@outlook.com>
2021-10-12 00:27:25 -0400

    feat: Add Ability to Interact with DataStores

--- src/Lowem/GeoserverPHP/DataStores.php
+++ src/Lowem/GeoserverPHP/DataStores.php
@@ -11,6 +11,149 @@ class DataStores extends Wrapper {
   /**
    * @throws HTTPRequestException
    */
+  public function getAll($workspaceName) {
+    if (empty($workspaceName)) {
+      try {
+        throw new MissingParamException("Sorry but \"workspaceName\" is a required field.", 404);
+      } catch (MissingParamException $e) {
+        echo $e->getMessage();
+        die($e->getCode());
+      }
+    }
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/$workspaceName/datastores");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $curl->get([
+      "Accept: application/json"
+    ]);
+    return $curl->getExecMessage();
+  }
+
+  /**
+   * @throws HTTPRequestException
+   */
+  public function create($data = []) {
+    $requireParams = [
+      "name",
+      "description",
+      "workspace"
+    ];
+    foreach ($requireParams as $value) {
+      if (!array_key_exists($value, $data) || empty($data[$value])) {
+        try {
+          throw new MissingParamException("Sorry but \"$value\" is a required field.", 404);
+        } catch (MissingParamException $e) {
+          echo $e->getMessage();
+          die($e->getCode());
+        }
+      }
+    }
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/{$data["workspace"]}/datastores");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+
+    $data = "{
+    \"dataStore\": {
+        \"name\": \"{$data["name"]}\",
+        \"description\": \"{$data["description"]}\",
+        \"connectionParameters\": {}
+    }
+  }";
+    $curl->post($data, [
+      "Content-Type: application/json",
+      "Accept: application/json"
+    ]);
+  }
+
+  /**
+   * @throws HTTPRequestException
+   */
+  public function get($workspaceName, $store) {
+    if (empty($workspaceName) || empty($store)) {
+      try {
+        throw new MissingParamException("Sorry but \"workspaceName\" and \"store\" are required fields.", 404);
+      } catch (MissingParamException $e) {
+        echo $e->getMessage();
+        die($e->getCode());
+      }
+    }
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/$workspaceName/datastores/$store");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $curl->get([
+      "Accept: application/json"
+    ]);
+    return $curl->getExecMessage();
+  }
+
+  /**
+   * @throws HTTPRequestException
+   */
+  public function update($workspaceName, $store, $data = []) {
+    $acceptedParams = [
+      "name",
+      "description",
+      "enabled",
+      "__default",
+    ];
+
+    if (empty($workspaceName) || empty($store)) {
+      try {
+        throw new MissingParamException("Sorry but \"workspaceName\" and \"store\" are required fields.", 404);
+      } catch (MissingParamException $e) {
+        echo $e->getMessage();
+        die($e->getCode());
+      }
+    }
+
+    foreach ($data as $key => $value) {
+      if (!in_array($key, $acceptedParams)) {
+        try {
+          throw new MissingParamException("Sorry but \"$key\" is not a valid field.", 404);
+        } catch (MissingParamException $e) {
+          echo $e->getMessage();
+          die($e->getCode());
+        }
+      }
+
+      if (empty($value)) {
+        unset($data[$key]);
+      }
+    }
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/$workspaceName/datastores/$store");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $jsonData = json_encode($data);
+    $data = "{
+      \"dataStore\": $jsonData
+    }";
+    $curl->put($data, [
+      "Content-Type: application/json",
+      "Accept: application/json"
+    ]);
+  }
+
+  /**
+   * @throws HTTPRequestException
+   */
+  public function delete($workspaceName, $store, $recurse = FALSE) {
+    if (empty($workspaceName) || empty($store)) {
+      try {
+        throw new MissingParamException("Sorry but \"workspaceName\" and \"store\" are required fields.", 404);
+      } catch (MissingParamException $e) {
+        echo $e->getMessage();
+        die($e->getCode());
+      }
+    }
+
+    $options = [
+      "recurse" => $recurse ? "true" : "false"
+    ];
+
+    $curl = new EasyCurl($this->getBaseURL() . "/workspaces/$workspaceName/coveragestores/$store");
+    $curl->setBasicAuth($this->getUsername(), $this->getPassword());
+    $curl->delete($options);
+  }
+
+  /**
+   * @throws HTTPRequestException
+   */
   public function shapeFileUpload($workspaceName, $store, $absFilePath) {
     if (empty($workspaceName) || empty($store) || empty($absFilePath)) {
       try {
--- tests/DataStoresTest.php
+++ tests/DataStoresTest.php
@@ -7,6 +7,68 @@ use PHPUnit\Framework\TestCase;
 class DataStoresTest extends TestCase {
   private $baseURL = "http://192.168.160.137:8080/geoserver";
 
+  public function testGetAll() {
+    $dataStore = new DataStores($this->baseURL);
+    $dataStore->setBasicAuth("admin", "geoserver");
+    $error = FALSE;
+    $result = "";
+    try {
+      $result = $dataStore->getAll("acme");
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error);
+    print_r($result);
+  }
+
+  public function testCreate() {
+    $dataStore = new DataStores($this->baseURL);
+    $dataStore->setBasicAuth("admin", "geoserver");
+    $error = FALSE;
+    try {
+      $dataStore->create([
+        "name" => "TestDataStore",
+        "description" => "This is a test store",
+        "workspace" => "acme"
+      ]);
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error);
+  }
+
+  public function testGet() {
+    $dataStore = new DataStores($this->baseURL);
+    $dataStore->setBasicAuth("admin", "geoserver");
+    $error = FALSE;
+    $result = "";
+    try {
+      $result = $dataStore->get("acme", "TestDataStore");
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error);
+    print_r($result);
+  }
+
+  public function testUpdate() {
+    $coverageStore = new DataStores($this->baseURL);
+    $coverageStore->setBasicAuth("admin", "geoserver");
+    $error = FALSE;
+    try {
+      $coverageStore->update("acme", "test123", [
+        "name" => "test142"
+      ]);
+    } catch (HTTPRequestException $e) {
+      $error = TRUE;
+      echo $e->getCustomMessage();
+    }
+    self::assertFalse($error);
+  }
+
   public function testShapeFileUpload() {
     $dataStore = new DataStores($this->baseURL);
     $dataStore->setBasicAuth("admin", "geoserver");
